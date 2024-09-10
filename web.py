136e33ff2459a9df6016ccc1464aa805a8b9904f
MoralesBilli <158227931+MoralesBilli@users.noreply.github.com>
2024-09-10 18:17:11 +0000

    Sitio web dinamico Se creo un diccioario en donde se garda la clave y la informacionq ue contiene, se realizo una condicion para que decidiera si se desplega la pagina homa o si se desplega la informacion del diccionario

--- web.py
+++ web.py
@@ -1,7 +1,25 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
 from urllib.parse import parse_qsl, urlparse
 
+contenido = {
+     '/proyecto/1': """<html>
+             <h1>Proyecto: 1</h1>
+             <head>
+             <meta charset="UTF-8" />
+             <meta name="viewport" content="width=device-width, initial-scale=1" />
+             <title>Programacion web</title>
+              <link href="css/style.css" rel="stylesheet" />
+             </head>
+             <p>Nombre: Billi Joel.</p>
+              </html>""",
+     '/proyecto/2': """<html>
+             <h1>Proyecto: 2</h1>
+             <p>Segunda pagina almacenada \t Numero de control: 21212002 </p>
+             </html>""",
+     '/proyecto/3': """<html>
+             <h1>Proyecto: 3</h1>
+             <p>Tercer contenido almacenado \t Carrera: Ingenieria en sistemas computacionales</p>
+             </html>"""}
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     def url(self):
@@ -12,10 +30,15 @@ class WebRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == "/":
+            conten = self.get_response()
+        else:
+            conten = contenido.get(self.path, None)
+
+        if conten:
             self.send_response(200)
             self.send_header("Content-Type", "text/html")
             self.end_headers()
-            self.wfile.write(self.get_response().encode("utf-8"))
+            self.wfile.write(conten.encode("utf-8"))
             
         else:
             self.send_response(404)
@@ -31,8 +54,8 @@ class WebRequestHandler(BaseHTTPRequestHandler):
                 return file.read()
         except Exception as e:
                 return f"<h1>Error al cargar la página: {e}</h1>"
-
-
+        
+    
 if __name__ == "__main__":
     print("Starting server")
     server = HTTPServer(("localhost", 8000), WebRequestHandler)
